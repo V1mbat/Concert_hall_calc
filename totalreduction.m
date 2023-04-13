d4a1933b44b0695cd7d053059e50fa824aae8fcc
V1mbat <119548577+V1mbat@users.noreply.github.com>
2023-04-13 14:45:22 +0200

    Update totalreduction.m

--- totalreduction.m
+++ totalreduction.m
@@ -6,14 +6,14 @@ clc
 wall1.rho = 700;                % density wall 1
 wall1.t = 0.3;                  % thickness wall 1
 wall1.m = wall1.rho * wall1.t;  % mass per unit area wall 1
-wall1.eta = 0.025;             % loss factor wall 1
+wall1.eta = 0.025;              % loss factor wall 1
 wall1.E = 10E9;                 % young's modulus wall 1
 wall1.nu = 0.3;                 % poisson ratio wall 1
 
 wall2.rho = 800;                % density wall 2
 wall2.t = 0.2;                  % thickness wall 2
 wall2.m = wall2.rho * wall2.t;  % mass per unit area wall 2
-wall2.eta = 0.025;             % loss factor wall 2
+wall2.eta = 0.025;              % loss factor wall 2
 wall2.E = 10E9;                 % young's modulus wall 2
 wall2.nu = 0.3;                 % poisson ratio wall 2
 
