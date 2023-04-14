608f445094ea0193a03e7ffea58cbfde6ddf551a
V1mbat <119548577+V1mbat@users.noreply.github.com>
2023-04-14 11:44:29 +0200

    added features

--- Estimations.m
+++ Estimations.m
@@ -1,41 +1,17 @@
 clc
 close all
 clear
-%% clarity
-V = 18000;
-T = 2;
-
-r = 1:0.1:50;
-
-E_direct = 100./r.^2;
-
-E_early = (31200*T/V)*exp(-0.04.*r./T)*(1-exp(-1.11/T));
-
-E_late = (31200*T/V)*exp(-0.04.*r./T)*exp(-1.11/T);
-
-C_80 = 10*log10((E_direct+E_early)./E_late);
-
-%% strength
-
-A = 0.163*V/T;      % equivilant absorption area
-Q = 1;              % directivity
-delta = 6.91/T;     % decay
-c = 340;            % speed of sound
-dir = Q./(4*pi*r.^2);
-diff = 4/A * exp(-2*delta*r./c);
-ref = Q/(4*pi*10^2);
-
-G = 10*log10((dir + diff)./ref);
-
-%% absorption layer
-f = 1:500;
-c = 340;
-lambda = c./f;
-
-abs = lambda./4;
+% to do:
+% add absorption to change T_60
+% done: T_60 of rehersal hall
+% done: C_80 and G dependant on T_60_calc
+% desired T_60 for rehersal room?
 
 %% reverb time and absorption of audience and seats
 % All tables are for the octave bands from 125 Hz to 4 kHz
+
+V = 18000;          %Volume
+c = 340;            % speed of sound
 
 f_oct = [125 250 500 1000 2000 4000];
 
@@ -43,34 +19,94 @@ area_peraudiencemember = 0.5*0.55;                  % area per seat
 
 area_audience = 2300 * area_peraudiencemember;      % area of whole audience
 
-L_facade = [73 75 80 66 54 50];                     % SPL at facade
-NC_20 = [40	33	26	22	19	17];                    % noise requirement in concert hall
 abs_seated = [0.62 0.72 0.8 0.83 0.84 0.85];        % absorption coeff seated
 abs_empty = [0.54 0.62 0.68 0.7 0.68 0.66];         % absorption coeff empty
 att_coeff = [0 0 0.0024 0.0042 0.0089 0.0262];      % attanuation coeff
 
+% reverb time when seated and empty
 T_60_calc_seated = 24*log(10)*V./(c*(4*att_coeff*V - area_audience*log(1-abs_seated)));
 T_60_calc_empty = 24*log(10)*V./(c*(4*att_coeff*V - area_audience*log(1-abs_empty)));
 
+%% clarity
+r = 1:0.1:50;
+
+E_direct = zeros(1,length(r));
+E_direct(:) = 100./r.^2;
+
+E_early_empty = zeros(length(T_60_calc_empty), length(r));
+E_late_empty = zeros(length(T_60_calc_empty), length(r));
+C_80_empty = zeros(length(T_60_calc_empty), length(r));
+
+E_early_occ = zeros(length(T_60_calc_seated), length(r));
+E_late_occ = zeros(length(T_60_calc_seated), length(r));
+C_80_occ = zeros(length(T_60_calc_seated), length(r));
+
+for i = 1:length(T_60_calc_empty)
+
+    E_early_empty(i,:) = (31200*T_60_calc_empty(i)/V)*exp(-0.04.*r./T_60_calc_empty(i))*(1-exp(-1.11/T_60_calc_empty(i)));
+    E_late_empty(i,:) = (31200*T_60_calc_empty(i)/V)*exp(-0.04.*r./T_60_calc_empty(i))*exp(-1.11/T_60_calc_empty(i));
+    C_80_empty(i,:) = 10*log10((E_direct+E_early_empty(i,:))./E_late_empty(i,:));
+
+    E_early_occ(i,:) = (31200*T_60_calc_seated(i)/V)*exp(-0.04.*r./T_60_calc_seated(i))*(1-exp(-1.11/T_60_calc_seated(i)));
+    E_late_occ(i,:) = (31200*T_60_calc_seated(i)/V)*exp(-0.04.*r./T_60_calc_seated(i))*exp(-1.11/T_60_calc_seated(i));
+    C_80_occ(i,:) = 10*log10((E_direct+E_early_empty(i,:))./E_late_empty(i,:));
+end
+
+%% strength
+
+A = 0.163*V./T_60_calc_seated;          % equivilant absorption area
+Q = 1;                                  % directivity
+delta = 6.91./T_60_calc_empty;          % decay
+ref = Q/(4*pi*10^2);
+dir = Q./(4*pi*r.^2);
+
+diff = zeros(length(A), length(r));
+G = zeros(length(A), length(r));
+
+for i = 1:length(A)
+    diff(i,:) = 4./A(i) * exp(-2*delta(i)*r./c);
+    G(i,:) = 10*log10((dir + diff(i,:))./ref);
+end
+
+%% rehersal room
+
+abs_area_person = [0.05 0.16 0.25 0.58 0.86 1.03];
+n_person = 100;                             % number of people in rehersal room
+
+V_rehersal = 20*10*5;                          % Volume of rehersal room
+area_wall = 2 * (20*10 + 20*5 + 10*5);      % area of the walls, floor and ceiling in rehersal room
+
+abs_wall = [0.2 0.18 0.15 0.12 0.1 0.1];    % absorption coefficient of wall
+abs_area_wall = area_wall*abs_wall;         % eq. absorption area of walls
+
+% reverberation time of rehersal room
+T_60_rehersal = 0.163*V_rehersal./(n_person*abs_area_person + abs_area_wall);
+
 %% plots
 
+% clarity
 figure
-plot(r, C_80)
+plot(r, C_80_occ)
+hold on
 thickenall_big;
 grid on
 xlabel('Distance r in m')
 ylabel('Clarity C_{80} in dB')
-ylim([-5 20])
+legend('125','250','500','1k','2k','4k')
+%ylim([-5 20])
 
+% strength
 figure
 plot(r, G)
 thickenall_big;
 grid on 
+legend('125','250','500','1k','2k','4k')
 xlabel('Distance r in m')
 ylabel('Strength G in dB')
 xlim([0 40])
 ylim([-5 20])
 
+% T_60
 figure
 semilogx(f_oct, T_60_calc_empty)
 hold on
--- totalreduction.m
+++ totalreduction.m
@@ -10,7 +10,7 @@ f_c = 10^3 * (2.^((-3:2)));
 %% double wall
 
 wall1.rho = 700;                % density wall 1
-wall1.t = 0.2;                  % thickness wall 1
+wall1.t = 0.3;                  % thickness wall 1
 wall1.m = wall1.rho * wall1.t;  % mass per unit area wall 1
 wall1.eta = 0.015;              % loss factor wall 1
 wall1.E = 10E9;                 % young's modulus wall 1
@@ -29,21 +29,28 @@ rho_0 = 1.2;                    % density air
 
 f = 1:10000;                    % frequency domain
 
-reduction_index1 = doublewall(f,wall1,wall2,d_z);
-
+r_doublewall = doublewall(f,wall1,wall2,d_z);
+% 
 reduction_index2 = singlewall(f,wall1);
-%%
+% 
 reduction_index3 = singlewall(f,wall2);
 
 %% lobby
-red_oct = mean_oct(f_c, reduction_index1);
+red_oct = mean_oct(f_c, r_doublewall);
+red2_oct = mean_oct(f_c, reduction_index2);
 
 S_lobby = 1100;                 % area of the outer wall of the lobby in m^2
 V_lobby = 12070;                % volume of lobby
 T_lobby = 1.0;                  % reverb time lobby
 A_lobby = 0.163*V_lobby/T_lobby;% equivilant absorption area in lobby
 
+S_hall = 1770;                  % area of the outer wall of the lobby in m^2
+V_hall = 18070;                 % volume of lobby
+T_hall = 2.0;                   % reverb time lobby
+A_hall = 0.163*V_hall/T_hall;   % equivilant absorption area in lobby
+
 L_lobby = L_facade + 3 - red_oct + 10*log10(S_lobby/A_lobby);
+L_hall = L_facade + 3 - red2_oct + 10*log10(S_hall/A_hall);
 
 %% plotting
 figure
@@ -57,10 +64,20 @@ ylabel('L_{SPL} in dB')
 legend('L_{Lobby}','NC_{20}')
 
 figure
-semilogx(f, reduction_index1)
+semilogx(f, r_doublewall)
 hold on
 semilogx(f, reduction_index2)
 semilogx(f, reduction_index3)
 legend('double wall', 'wall 1', 'wall 2')
 thickenall_big
+
+figure
+semilogx(f_c, L_hall)
+hold on
+grid on 
+semilogx(f_c, NC_20)
+thickenall_big
+xlabel('f in Hz')
+ylabel('L_{SPL} in dB')
+legend('L_{Hall}','NC_{20}')
 
