bad21ffa3d67b4c15d1b757d49106190297035af
V1mbat <119548577+V1mbat@users.noreply.github.com>
2023-08-14 13:30:46 +0200

    some more stuff

--- totalreduction.m
+++ totalreduction.m
@@ -17,7 +17,7 @@ rho_0 = 1.2;                    % density air
 % the format of the data should be like below
 
 % wall 1
-wall1.rho = 900;               % density wall 1
+wall1.rho = 900;                % density wall 1
 wall1.t = 0.15;                 % thickness wall 1
 wall1.m = wall1.rho * wall1.t;  % mass per unit area wall 1
 wall1.eta = 0.025;              % loss factor wall 1
@@ -34,8 +34,8 @@ wall2.nu = 0.3;                 % poisson ratio wall 2
 
 % glass 1
 glass1.rho = 2500;               % density wall 3
-glass1.t = 0.0042;                % thickness wall 3
-glass1.m = glass1.rho * glass1.t;  % mass per unit area wall 3
+glass1.t = 0.0042;               % thickness wall 3
+glass1.m = glass1.rho * glass1.t;% mass per unit area wall 3
 glass1.eta = 1E-4;               % loss factor wall 3
 glass1.E = 80E9;                 % young's modulus wall 3
 glass1.nu = 0.3;                 % poisson ratio wall 3
@@ -43,15 +43,15 @@ glass1.nu = 0.3;                 % poisson ratio wall 3
 % glass 2
 glass2.rho = 2500;               % density wall 1
 glass2.t = 0.015;                % thickness wall 1
-glass2.m = glass2.rho * glass2.t;  % mass per unit area wall 1
+glass2.m = glass2.rho * glass2.t;% mass per unit area wall 1
 glass2.eta = 1E-4;               % loss factor wall 3
 glass2.E = 80E9;                 % young's modulus wall 3
 glass2.nu = 0.3;                 % poisson ratio wall 3
 
 % glass 3
 glass3.rho = 2500;               % density wall 1
-glass3.t = 0.0035;                % thickness wall 1
-glass3.m = glass3.rho * glass3.t;  % mass per unit area wall 1
+glass3.t = 0.0035;               % thickness wall 1
+glass3.m = glass3.rho * glass3.t;% mass per unit area wall 1
 glass3.eta = 1E-4;               % loss factor wall 3
 glass3.E = 80E9;                 % young's modulus wall 3
 glass3.nu = 0.3;                 % poisson ratio wall 3
@@ -59,7 +59,7 @@ glass3.nu = 0.3;                 % poisson ratio wall 3
 % glass 4
 glass4.rho = 2500;               % density wall 1
 glass4.t = 0.012;                % thickness wall 1
-glass4.m = glass4.rho * glass4.t;  % mass per unit area wall 1
+glass4.m = glass4.rho * glass4.t;% mass per unit area wall 1
 glass4.eta = 1E-4;               % loss factor wall 3
 glass4.E = 80E9;                 % young's modulus wall 3
 glass4.nu = 0.3;                 % poisson ratio wall 3
@@ -67,7 +67,7 @@ glass4.nu = 0.3;                 % poisson ratio wall 3
 
 % distance between the walls
 d_1 = 0.25;                     % distance between wall 1 and wall 2
-d1_glass = 0.004;                % distance between the glass panels
+d1_glass = 0.004;               % distance between the glass panels
 d2_glass = 0.006;
 d_34 = 0.25;                    % distance between wall 3 and wall 4
 
